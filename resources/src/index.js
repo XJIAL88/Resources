92db9418c5ec05da9c9733b18f50e6c056c3e800
XJIAL88 <787995918>
2019-08-07 10:37:54 +0800

    资源管理

--- resources/src/index.js
+++ resources/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*import*/
+// import Header from './routes/Header/Header';
+// import Sidebar from './routes/Sidebar/Sidebar';
+// import Subside from './routes/Subside/Subside';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<main>
+    {/*<Header/>*/}
+    {/*<Sidebar/>*/}
+    {/*<Subside/>*/}
+</main>, document.getElementById('root'));
 
 
